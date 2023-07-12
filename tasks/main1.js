41b33fef76639194d5323608b12664dad016a896
IdreesDweik <idrees.dweik@gmail.com>
2023-07-12 14:33:58 +0300

    JS

--- tasks/main1.js
+++ tasks/main1.js
@@ -1,5 +1,11 @@
 // idrees 
 
 function tellFortune(){
-    
+    const numberOfChildren = 3 ;
+    const partnerName = "sarah" ;
+    const geographicLocation = "japan" ;
+    const jobTitle  = "Business man" ;
+    console.log("You will be a " + jobTitle + " in " + geographicLocation + " , and married to " + partnerName + " with " + numberOfChildren + " kids.");
 }
+
+tellFortune();
