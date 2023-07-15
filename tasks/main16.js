6d56ffe30ff4405d3a5cd9703a29c9e9ae59ff46
IdreesDweik <idrees.dweik@gmail.com>
2023-07-15 15:19:44 +0300

    JS

---
+++ tasks/main16.js
@@ -0,0 +1,15 @@
+// idrees
+
+/*
+function isEven(){
+
+    let num = parseFloat(prompt("Please enter your number"));
+
+    if( (num % 2) === 0){
+        console.log("false");
+    } else{
+        console.log("true");
+    }
+}
+isEven();
+*/
